1ee9db8c03c7d90ad20aff1fbe467be1e356a43a
tgardner1959 <tgardner1959@gmail.com>
2012-05-03 15:18:47 +0000

    moved to samplecode.password

--- src/main/java/samplecode/PasswordModifyExtendedOperationFailedException.java
+++ src/main/java/samplecode/PasswordModifyExtendedOperationFailedException.java
@@ -15,10 +15,8 @@
  */
 package samplecode;
 
-
 import com.unboundid.ldap.sdk.ResultCode;
 import com.unboundid.util.Validator;
-
 
 /**
  * thrown when the password modify extended operation failed.
@@ -27,37 +25,31 @@ import com.unboundid.util.Validator;
 @Since("Dec 28, 2011")
 @CodeVersion("1.0")
 @SuppressWarnings("serial")
-public class PasswordModifyExtendedOperationFailedException
-  extends Exception
-{
+public class PasswordModifyExtendedOperationFailedException extends Exception {
 
+    private final ResultCode resultCode;
 
-  private final ResultCode resultCode;
+    /**
+     * retrieves the result code associated with the exception.
+     * 
+     * @return result code from the exception.
+     */
+    public ResultCode getResultCode() {
+	return this.resultCode;
+    }
 
-
-  /**
-   * retrieves the result code associated with the exception.
-   * 
-   * @return result code from the exception.
-   */
-  public ResultCode getResultCode()
-  {
-    return this.resultCode;
-  }
-
-
-  /**
-   * Constructs the object and sets its state with the
-   * {@code resultCode} parameter.
-   * 
-   * @param resultCode
-   *          The result code from a failed password modify extended
-   *          operation failed exception.
-   */
-  public PasswordModifyExtendedOperationFailedException(final ResultCode resultCode)
-  {
-    Validator.ensureNotNull(resultCode);
-    this.resultCode = resultCode;
-  }
+    /**
+     * Constructs the object and sets its state with the {@code resultCode}
+     * parameter.
+     * 
+     * @param resultCode
+     *            The result code from a failed password modify extended
+     *            operation failed exception.
+     */
+    public PasswordModifyExtendedOperationFailedException(
+	    final ResultCode resultCode) {
+	Validator.ensureNotNull(resultCode);
+	this.resultCode = resultCode;
+    }
 
 }
