53007b2c7a49185c9fe1c64e6031c66c69fc4c1b
tgardner1959 <tgardner1959@gmail.com>
2012-06-17 13:48:34 +0000

    added annotations.

--- src/main/java/samplecode/exception/ExceptionMsgFactory.java
+++ src/main/java/samplecode/exception/ExceptionMsgFactory.java
@@ -6,7 +6,15 @@ import com.unboundid.ldap.sdk.ResultCode;
 import com.unboundid.util.Validator;
 
 
+import samplecode.annotation.Author;
+import samplecode.annotation.CodeVersion;
+import samplecode.annotation.Since;
+
+
 /** Provides support for creating exception message generators */
+@Author("terry.gardner@unboundID.com")
+@Since("Jun 17, 2012")
+@CodeVersion("1.0")
 public final class ExceptionMsgFactory
 {
 
