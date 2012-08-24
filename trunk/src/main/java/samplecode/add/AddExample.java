503b76c8248f407c9d8ac527da7ef7ec4b19a0bb
tgardner1959@gmail.com <tgardner1959@gmail.com@06629ffb-738d-6adf-8147-5ade73cfec28>
2012-08-24 16:51:17 +0000

    Refactored this simple example to close the LDAPConnection.

--- trunk/src/main/java/samplecode/add/AddExample.java
+++ trunk/src/main/java/samplecode/add/AddExample.java
@@ -9,41 +9,57 @@ import com.unboundid.ldap.sdk.LDAPResult;
 import com.unboundid.ldif.LDIFException;
 
 
-/** demonstrate the {@link AddRequest} */
+/**
+ * demonstrate the {@link AddRequest}
+ */
 public final class AddExample
 {
 
   /**
    * demonstrate the {@link AddRequest}
-   * 
-   * @throws LDIFException
-   * @throws LDAPException
    */
-  public static void main(final String... args) throws LDAPException,LDIFException
+  public static void main(final String... args)
   {
     /*
-     * Use connection options to specify that the connection attempt
-     * should be 1 second and if the ADD request times out, the request
-     * should be abandoned
-     */
+    * Use connection options to specify that the connection attempt
+    * should be 1 second and if the ADD request times out, the request
+    * should be abandoned
+    */
     final LDAPConnectionOptions connectionOptions = new LDAPConnectionOptions();
     connectionOptions.setAbandonOnTimeout(true);
     connectionOptions.setConnectTimeoutMillis(1000);
 
     /*
-     * LDIF lines for the add request
-     */
+    * LDIF lines for the add request
+    */
     final String[] ldifLines =
-            {
-                    "dn: uid=user,dc=example,dc=com", "changetype: add", "cn: Joe User",
-                    "sn: User", "uid: user", "userPassword: password"
-            };
+            {"dn: uid=user,dc=example,dc=com", "changetype: add", "cn: Joe User", "sn: User",
+                    "uid: user", "userPassword: password"};
 
     final String host = "ldap.example.com";
     final int port = 389;
+    try
+    {
+      final LDAPConnection ldapConnection = new LDAPConnection(connectionOptions, host, port);
+      try
+      {
+        final LDAPResult ldapResult = ldapConnection.add(new AddRequest(ldifLines));
+        System.out.println(ldapResult);
+      }
+      catch(LDIFException e)
+      {
+        System.err.println(e);
+      }
+      finally
+      {
+        ldapConnection.close();
+      }
+    }
+    catch(LDAPException e)
+    {
+      System.err.println(e);
+    }
 
-    final LDAPResult ldapResult =
-            new LDAPConnection(connectionOptions,host,port).add(new AddRequest(ldifLines));
-    System.out.println(ldapResult);
   }
+
 }
