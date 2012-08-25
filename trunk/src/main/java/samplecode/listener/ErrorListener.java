cd49e8b0bdfb5f48d633a6ed924ba095722c6568
tgardner1959@gmail.com <tgardner1959@gmail.com@06629ffb-738d-6adf-8147-5ade73cfec28>
2012-08-25 19:17:53 +0000

    Corrected warning in javadoc

--- trunk/src/main/java/samplecode/listener/ErrorListener.java
+++ trunk/src/main/java/samplecode/listener/ErrorListener.java
@@ -15,37 +15,29 @@
  */
 package samplecode.listener;
 
-
-import java.io.PrintStream;
-import java.util.EventListener;
-
-
 import samplecode.annotation.Author;
 import samplecode.annotation.CodeVersion;
 import samplecode.annotation.Since;
 
+import java.io.PrintStream;
+import java.util.EventListener;
 
 /**
  * Notified when an error or exception occurs at the discretion of the
  * client and implementor.
- * 
- * @param <T>
- *          the type of data transmitted to {@ode displayError}.
+ *
+ * @param <T> the type of data transmitted to {@code displayError}.
  */
-@Author("terry.gardner@unboundid.com")
-@Since("Dec 26, 2011")
-@CodeVersion("1.0")
-public interface ErrorListener<T>
-        extends EventListener
+@Author("terry.gardner@unboundid.com") @Since("Dec 26, 2011") @CodeVersion("1.0")
+public interface ErrorListener<T> extends EventListener
 {
 
   /**
    * Displays an error message in a manner decided by the implementor.
-   * 
-   * @param errStream
-   *          a print stream to which the error message is transmitted.
-   * @param object
-   *          the object that generated the error.
+   *
+   * @param errStream a print stream to which the error message is transmitted.
+   * @param object    the object that generated the error.
    */
-  void displayError(PrintStream errStream,T object);
+  void displayError(PrintStream errStream, T object);
+
 }
