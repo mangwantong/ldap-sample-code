abb69f335c2f8686d46dd38f797e13013e305487
tgardner1959 <tgardner1959@gmail.com>
2013-02-21 19:20:23 +0000

    Added some code to executeToolTasks to create server sets.

--- src/main/java/samplecode/ha/ServerSetDemo.java
+++ src/main/java/samplecode/ha/ServerSetDemo.java
@@ -3,19 +3,28 @@
  */
 package samplecode.ha;
 
-import com.unboundid.ldap.sdk.ResultCode;
+import com.unboundid.ldap.sdk.*;
 import com.unboundid.util.args.ArgumentException;
 import com.unboundid.util.args.ArgumentParser;
 import com.unboundid.util.args.StringArgument;
 import samplecode.tools.AbstractTool;
 import samplecode.tools.BasicToolCompletedProcessing;
 import samplecode.tools.ToolCompletedProcessing;
+import samplecode.util.SampleCodeCollectionUtils;
 
 import java.io.PrintStream;
+import java.util.List;
+
+import static samplecode.util.SampleCodeCollectionUtils.newArrayList;
 
 public class ServerSetDemo extends AbstractTool {
 
-    private static final String CLASS_SPECIFIC_PROPERTIES_RESOURCE_NAME = "ServerSetDemo.properties";
+    /**
+     * The name of the properties file for this tool.
+     */
+    private static final String CLASS_SPECIFIC_PROPERTIES_RESOURCE_NAME =
+            "ServerSetDemo.properties";
+
     private StringArgument serverUrlStringArgument;
 
     private ServerSetDemo(final PrintStream out, final PrintStream err) {
@@ -26,7 +35,8 @@ public class ServerSetDemo extends AbstractTool {
         main(System.out,System.err,args);
     }
 
-    private static void main(final PrintStream out, final PrintStream err, String... args) {
+    private static void main(final PrintStream out,
+                             final PrintStream err, String... args) {
         final ServerSetDemo serverSetDemo = new ServerSetDemo(out,err);
         final ResultCode resultCode = serverSetDemo.runTool(args);
         final ToolCompletedProcessing completedProcessing =
@@ -36,15 +46,18 @@ public class ServerSetDemo extends AbstractTool {
 
     /**
      * Adds the arguments needed by this command-line tool to the provided
-     * argument parser which are not related to connecting or authenticating to
+     * argument parser which are not related to connecting or
+     * authenticating to
      * the directory server.
      *
-     * @param parser The argument parser to which the arguments should be added.
+     * @param parser The argument parser to which the arguments should be
+     *               added.
      * @throws com.unboundid.util.args.ArgumentException
      *          If a problem occurs while adding the arguments.
      */
     @Override
-    public void addArguments(final ArgumentParser parser) throws ArgumentException {
+    public void addArguments(final ArgumentParser parser) throws
+            ArgumentException {
 
         String description;
         String longName;
@@ -54,7 +67,8 @@ public class ServerSetDemo extends AbstractTool {
         int maxOccurrences;
 
 
-        // Add the argument whose value is the URL of a server to use in the server set
+        // Add the argument whose value is the URL of a server to use in
+        // the server set
         shortName = null;
         longName = "server-url";
         isRequired = true;
@@ -62,7 +76,8 @@ public class ServerSetDemo extends AbstractTool {
         placeHolder = "{URL}";
         description = "The URL of a server to use in the server set.";
         serverUrlStringArgument =
-                new StringArgument(shortName,longName,isRequired,maxOccurrences,placeHolder,
+                new StringArgument(shortName,longName,isRequired,
+                        maxOccurrences,placeHolder,
                         description);
         parser.addArgument(serverUrlStringArgument);
     }
@@ -80,6 +95,32 @@ public class ServerSetDemo extends AbstractTool {
      */
     @Override
     protected ResultCode executeToolTasks() {
-        return ResultCode.SUCCESS;  //To change body of implemented methods use File | Settings | File Templates.
+        final List<String> serverUrls =
+                serverUrlStringArgument.getValues();
+        final List<LDAPURL> ldapUrls = newArrayList();
+        for(final String serverUrl : serverUrls) {
+            final LDAPURL ldapUrl;
+            try {
+                ldapUrl = new LDAPURL(serverUrl);
+                ldapUrls.add(ldapUrl);
+            } catch(LDAPException e) {
+                getLogger().error(e);
+            }
+        }
+        final List<ServerSet> serverSets = newArrayList();
+        for(LDAPURL ldapUrl:ldapUrls) {
+            final ServerSet serverSet = new SingleServerSet(ldapUrl
+                    .getHost(),ldapUrl.getPort());
+            serverSets.add(serverSet);
+        }
+        final ServerSet serverSet =
+                new FailoverServerSet(serverSets);
+        try {
+            serverSet.getConnection();
+        } catch(LDAPException e) {
+            getLogger().error(e);
+        }
+        return ResultCode.SUCCESS;  //To change body of implemented
+        // methods use File | Settings | File Templates.
     }
 }
