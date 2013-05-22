c57bb935f6514080c5c70ca2e86d0065d5a87052
tgardner1959 <tgardner1959@gmail.com>
2013-05-22 10:07:25 +0000

    Added copyright notice. Shortened some lines. Clarified with key assignments.

--- src/main/java/samplecode/args/BooleanPropertiesBackedArgument.java
+++ src/main/java/samplecode/args/BooleanPropertiesBackedArgument.java
@@ -1,12 +1,28 @@
+/*
+ * Copyright 2008-2011 UnboundID Corp. All Rights Reserved.
+ */
+/*
+ * Copyright (C) 2008-2011 UnboundID Corp. This program is free
+ * software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License (GPLv2 only) or the terms of the GNU
+ * Lesser General Public License (LGPLv2.1 only) as published by the
+ * Free Software Foundation. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+
 package samplecode.args;
 
 
-import com.unboundid.util.Validator;
 import com.unboundid.util.args.ArgumentException;
 import com.unboundid.util.args.BooleanArgument;
 
-
 import java.util.ResourceBundle;
+
+import static com.unboundid.util.Validator.ensureNotNull;
 
 
 /**
@@ -14,52 +30,54 @@ import java.util.ResourceBundle;
  * an {@link Boolean}.
  */
 public class BooleanPropertiesBackedArgument
-        extends PropertiesBackedArgument<BooleanArgument,Boolean>
-{
+        extends PropertiesBackedArgument<BooleanArgument,Boolean> {
+
+
+
+  private BooleanPropertiesBackedArgument(
+          final ResourceBundle resourceBundle, final String basePropertyName) {
+    super(resourceBundle,basePropertyName);
+  }
 
 
 
   /**
    * @param resourceBundle
-   *          the resource bundle from which the command line argument
-   *          are taken
-   * 
+   *         the resource bundle from which the command line argument
+   *         are taken
    * @param basePropertyName
-   *          the base property name
-   * 
+   *         the base property name
+   *
    * @return a new {@code StringPropertiesBackedArgument} object.
    */
-  public static BooleanPropertiesBackedArgument newBooleanPropertiesBackedArgument(
-          final ResourceBundle resourceBundle,final String basePropertyName)
-  {
+  public static BooleanPropertiesBackedArgument
+    newBooleanPropertiesBackedArgument(
+          final ResourceBundle resourceBundle, final String basePropertyName) {
     return new BooleanPropertiesBackedArgument(resourceBundle,basePropertyName);
   }
 
 
 
+  /**
+   * @return the {@code value} as an {@link Integer}.
+   */
   @Override
-  public BooleanArgument getArgument() throws ArgumentException
-  {
-    return new BooleanArgument(getShortIdentifier(),getLongIdentifier(),getMaxOccurrences(),
-            getDescription());
-  }
+  protected Boolean convertString(final String value) {
+    ensureNotNull(value);
 
-
-
-  /** @return the {@code value} as an {@link Integer}. */
-  @Override
-  protected Boolean convertString(final String value)
-  {
-    Validator.ensureNotNull(value);
     return Boolean.valueOf(value);
   }
 
 
 
-  private BooleanPropertiesBackedArgument(
-          final ResourceBundle resourceBundle,final String basePropertyName)
-  {
-    super(resourceBundle,basePropertyName);
+
+
+
+
+  @Override
+  public BooleanArgument getArgument() throws ArgumentException {
+    return new BooleanArgument(getShortIdentifier(),getLongIdentifier(),
+                               getMaxOccurrences(),getDescription());
   }
 
 
