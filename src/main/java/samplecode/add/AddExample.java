0aefce4c9178cbeed0cf7406a5df79fd09d12c2f
tgardner1959 <tgardner1959@gmail.com>
2013-07-28 11:04:48 +0000

    Cleaned up the AddExample.

--- src/main/java/samplecode/add/AddExample.java
+++ src/main/java/samplecode/add/AddExample.java
@@ -23,12 +23,23 @@ import com.unboundid.ldap.sdk.LDAPConnectionOptions;
 import com.unboundid.ldap.sdk.LDAPException;
 import com.unboundid.ldap.sdk.LDAPResult;
 import com.unboundid.util.LDAPTestUtils;
+import com.unboundid.util.ssl.SSLUtil;
+import com.unboundid.util.ssl.TrustAllTrustManager;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.TrustManager;
+import java.security.GeneralSecurityException;
 
 
 /**
  * Demonstrates the {@link AddRequest}; this example uses a hard coded
- * hostname of {@code ldap-server.ldap.com} and port {@code 389} and
- * uses the test utilities class to generate an entry to add.
+ * hostname of {@code centos.example.com} and port {@code 1636} and
+ * uses the test utilities class to generate an entry to add. The example
+ * uses an unauthenticated connection to attempt the ADD. If an
+ * unauthenticated connection does have sufficient access permissions to add
+ * the entry, simply add some code to use an authenticated connection that
+ * does have permission.
  */
 public final class AddExample {
 
@@ -42,23 +53,33 @@ public final class AddExample {
     // should be abandoned.
     LDAPConnectionOptions connectionOptions = new LDAPConnectionOptions();
     connectionOptions.setAbandonOnTimeout(true);
-    connectionOptions.setConnectTimeoutMillis(AddExample.OP_TIMEOUT_MILLIS);
+    connectionOptions.setConnectTimeoutMillis((int)AddExample.TIMEOUT_MILLIS);
 
     String host = AddExample.HOSTNAME;
     int port = AddExample.PORT;
     int result;
     LDAPResult ldapResult = null;
+
+    KeyManager keyManager = null;
+    TrustManager trustManager = new TrustAllTrustManager();
+    SSLUtil sslUtil = new SSLUtil(keyManager,trustManager);
+
     try {
+
+      SocketFactory socketFactory = sslUtil.createSSLSocketFactory();
 
       // Connect to the server.
       LDAPConnection ldapConnection =
-        new LDAPConnection(connectionOptions,host,port);
+        new LDAPConnection(socketFactory,connectionOptions,host,port);
       try {
 
         Entry entry = LDAPTestUtils.generateUserEntry(GIVEN_NAME,
           BASE_OBJECT,GIVEN_NAME,SURNAME,PASSWORD);
 
+        // Add the entry (abandon the attempt if it does not succeed in the
+        // specified number of milliseconds).
         AddRequest addRequest = new AddRequest(entry);
+        addRequest.setResponseTimeoutMillis(AddExample.TIMEOUT_MILLIS);
 
         // Transmit the AddRequest to the server.
         ldapResult = ldapConnection.add(addRequest);
@@ -66,29 +87,28 @@ public final class AddExample {
         System.out.println(ldapResult);
       } finally {
         ldapConnection.close();
-
-        // Convert the result code to an integer for use in the exit method.
-        result = ldapResult == null ? 1 : ldapResult.getResultCode().intValue();
       }
     } catch(final LDAPException e) {
-      System.err.println(String.valueOf(AddExample.class) + " is an example " +
-        "of how to add an entry to an LDAP Directory Server. The example is " +
-        "primitive and simple minded: it expects the LDAP Directory Server " +
-        "to be listening on ldap://localhost:389 and hosting 'dc=example," +
-        "dc=com'.");
-      e.printStackTrace();
+
+      System.err.println(e);
       result = e.getResultCode().intValue();
+      if(result != 0) {
+        System.exit(result);
+      }
+
+    } catch(GeneralSecurityException e) {
+
+      System.err.println(e);
+      System.exit(1);
+
     }
-
-    System.exit(result);
   }
-
 
 
   /**
    * The object that is immediately superior to the user objects.
    */
-  private static final String BASE_OBJECT = "ou=people,c=us";
+  private static final String BASE_OBJECT = "ou=people,dc=example,dc=com";
 
 
   /**
@@ -100,14 +120,7 @@ public final class AddExample {
   /**
    * The host where the LDAP Directory Server listens for client connections
    */
-  private static final String HOSTNAME = "ldap-server.ldap.com";
-
-
-  /**
-   * The maximum number of milliseconds to wait before abandoning the
-   * AddRequest
-   */
-  private static final int OP_TIMEOUT_MILLIS = 1000;
+  private static final String HOSTNAME = "centos.example.com";
 
 
   /**
@@ -118,15 +131,22 @@ public final class AddExample {
 
   /**
    * The port on which the server at {@code HOSTNAME} listens for client
-   * connections
+   * connections - this is expected to be th secure port.
    */
-  private static final int PORT = 389;
+  private static final int PORT = 1636;
 
 
   /**
    * Bab's last name
    */
   private static final String SURNAME = "Jensen";
+
+
+  /**
+   * The maximum number of milliseconds to wait before abandoning the
+   * attempt to connect or ADD.
+   */
+  private static final long TIMEOUT_MILLIS = 1000;
 
 
 }
