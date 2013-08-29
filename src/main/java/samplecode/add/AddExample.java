b284b049edd0d362485cad802d7208ceaf607e48
tgardner1959 <tgardner1959@gmail.com>
2013-08-29 10:24:41 +0000

    Moved functionality out of main into AddExample

--- src/main/java/samplecode/add/AddExample.java
+++ src/main/java/samplecode/add/AddExample.java
@@ -16,12 +16,7 @@
 
 package samplecode.add;
 
-import com.unboundid.ldap.sdk.AddRequest;
-import com.unboundid.ldap.sdk.Entry;
-import com.unboundid.ldap.sdk.LDAPConnection;
-import com.unboundid.ldap.sdk.LDAPConnectionOptions;
-import com.unboundid.ldap.sdk.LDAPException;
-import com.unboundid.ldap.sdk.LDAPResult;
+import com.unboundid.ldap.sdk.*;
 import com.unboundid.util.LDAPTestUtils;
 import com.unboundid.util.ssl.SSLUtil;
 import com.unboundid.util.ssl.TrustAllTrustManager;
@@ -30,7 +25,6 @@ import javax.net.SocketFactory;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.TrustManager;
 import java.security.GeneralSecurityException;
-
 
 /**
  * Demonstrates the {@link AddRequest}; this example uses a hard coded
@@ -41,80 +35,34 @@ import java.security.GeneralSecurityException;
  * the entry, simply add some code to use an authenticated connection that
  * does have permission.
  */
-public final class AddExample {
-
-  /**
-   * demonstrate the {@link AddRequest}
-   */
-  public static void main(final String... args) {
-
-    // Use connection options to specify that the connection attempt
-    // should be 1 second and if the ADD request times out, the request
-    // should be abandoned.
-    LDAPConnectionOptions connectionOptions = new LDAPConnectionOptions();
-    connectionOptions.setAbandonOnTimeout(true);
-    connectionOptions.setConnectTimeoutMillis((int)AddExample.TIMEOUT_MILLIS);
-
-    String host = AddExample.HOSTNAME;
-    int port = AddExample.PORT;
-    int result;
-    LDAPResult ldapResult = null;
-
-    KeyManager keyManager = null;
-    TrustManager trustManager = new TrustAllTrustManager();
-    SSLUtil sslUtil = new SSLUtil(keyManager,trustManager);
-
-    try {
-
-      SocketFactory socketFactory = sslUtil.createSSLSocketFactory();
-
-      // Connect to the server.
-      LDAPConnection ldapConnection =
-        new LDAPConnection(socketFactory,connectionOptions,host,port);
-      try {
-
-        Entry entry = LDAPTestUtils.generateUserEntry(GIVEN_NAME,
-          BASE_OBJECT,GIVEN_NAME,SURNAME,PASSWORD);
-
-        // Add the entry (abandon the attempt if it does not succeed in the
-        // specified number of milliseconds).
-        AddRequest addRequest = new AddRequest(entry);
-        addRequest.setResponseTimeoutMillis(AddExample.TIMEOUT_MILLIS);
-
-        // Transmit the AddRequest to the server.
-        ldapResult = ldapConnection.add(addRequest);
-
-        System.out.println(ldapResult);
-      } finally {
-        ldapConnection.close();
-      }
-    } catch(final LDAPException e) {
-
-      System.err.println(e);
-      result = e.getResultCode().intValue();
-      if(result != 0) {
-        System.exit(result);
-      }
-
-    } catch(GeneralSecurityException e) {
-
-      System.err.println(e);
-      System.exit(1);
-
-    }
-  }
-
-
-  /**
-   * The object that is immediately superior to the user objects.
-   */
-  private static final String BASE_OBJECT = "ou=people,dc=example,dc=com";
+public final class AddExample
+{
 
 
   /**
    * Bab's given name.
    */
   private final static String GIVEN_NAME = "Babs";
+
+
+  /**
+   * The port on which the server at {@code HOSTNAME} listens for client
+   * connections - this is expected to be th secure port.
+   */
+  private static final int PORT = 1636;
+
+
+  /**
+   * The maximum number of milliseconds to wait before abandoning the
+   * attempt to connect or ADD.
+   */
+  private static final long TIMEOUT_MILLIS = 1000;
+
+
+  /**
+   * The object that is immediately superior to the user objects.
+   */
+  private static final String BASE_OBJECT = "ou=people,dc=example,dc=com";
 
 
   /**
@@ -130,23 +78,95 @@ public final class AddExample {
 
 
   /**
-   * The port on which the server at {@code HOSTNAME} listens for client
-   * connections - this is expected to be th secure port.
-   */
-  private static final int PORT = 1636;
-
-
-  /**
    * Bab's last name
    */
   private static final String SURNAME = "Jensen";
 
 
+  private String hostname;
+
+
+  private int port;
+
+
+  AddExample()
+  {
+    this(HOSTNAME, PORT);
+  }
+
+
+  AddExample(String hostname, int port)
+  {
+    this.hostname = hostname;
+    this.port = port;
+  }
+
+
   /**
-   * The maximum number of milliseconds to wait before abandoning the
-   * attempt to connect or ADD.
+   * demonstrate the {@link AddRequest}
    */
-  private static final long TIMEOUT_MILLIS = 1000;
+  public static void main(final String... args)
+  {
+    try
+    {
+      AddExample addExample = new AddExample();
+      LDAPResult ldapResult = addExample.addEntry();
+      if (!ldapResult.getResultCode().equals(ResultCode.SUCCESS))
+      {
+        System.exit(ldapResult.getResultCode().intValue());
+      }
+    }
+    catch(LDAPException ldapException)
+    {
+      System.err.printf("Unable to add the entry: %s", ldapException);
+    }
+    catch(GeneralSecurityException gse)
+    {
+      System.err.printf("Unable to connect to server: %s", gse);
+    }
+  }
+
+
+  LDAPConnection getConnection() throws LDAPException, GeneralSecurityException
+  {
+    SocketFactory socketFactory = getSocketFactory();
+    return new LDAPConnection(socketFactory, getLDAPConnectionOptions(),
+      hostname, port);
+  }
+
+
+  SocketFactory getSocketFactory() throws GeneralSecurityException
+  {
+    KeyManager keyManager = null;
+    TrustManager trustManager = new TrustAllTrustManager();
+    SSLUtil sslUtil = new SSLUtil(keyManager, trustManager);
+    return sslUtil.createSSLSocketFactory();
+  }
+
+
+  LDAPConnectionOptions getLDAPConnectionOptions()
+  {
+    LDAPConnectionOptions connectionOptions = new LDAPConnectionOptions();
+    connectionOptions.setAbandonOnTimeout(true);
+    connectionOptions.setConnectTimeoutMillis((int) AddExample.TIMEOUT_MILLIS);
+    return connectionOptions;
+  }
+
+
+  LDAPResult addEntry() throws LDAPException, GeneralSecurityException
+  {
+    LDAPConnection conn = getConnection();
+    LDAPResult result = conn.add(getAddRequest());
+    conn.close();
+    return result;
+  }
+
+
+  AddRequest getAddRequest()
+  {
+    return new AddRequest(LDAPTestUtils.generateUserEntry(GIVEN_NAME,
+      BASE_OBJECT, GIVEN_NAME, SURNAME, PASSWORD));
+  }
 
 
 }
